c238efb13b8c716f9078730b07ca392a3a34153e
kishan <jaiswalkishan0010@gmail.com>
2024-09-28 02:57:00 +0545

    use helvetica

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
         "custom-gradient":
           "linear-gradient(180deg, rgba(13, 8, 13, 0.18) 0%, rgba(121, 91, 212, 0.21) 50%, #0A0A0A 100%)",
       },
+      fontFamily: {
+        sans: ["Helvetica", "Arial", "sans-serif"], // Set Helvetica as the default sans font
+      },
     },
-  },
-  theme: {
-    extend: {},
   },
   plugins: [],
 };
