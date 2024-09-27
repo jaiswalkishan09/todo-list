39bc89ad9c6e35b54daf12c351f7f4deb949d716
kishan <jaiswalkishan0010@gmail.com>
2024-09-27 17:54:49 +0545

    dashboard1 is in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Dashbord from "./components/Dashbord";
 
 function App() {
   return (
-    <div className="overflow-auto h-screen bg-gradient-to-b from-[#0d080d2e] via-[#795bd42e] to-[#0a0a0a]">
+    <div className="overflow-auto h-screen bg-gradient-to-b from-[#09051b] to-[#0a061d]">
       <div className="w-screen flex justify-center pt-[16px]">
         <Header />
+      </div>
+      <div>
+        <div className="mt-[102px] ml-[100px]">
+          <Dashbord />
+        </div>
       </div>
     </div>
   );
